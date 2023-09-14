db1fa9da44a65e4187be98f172a99a7ab2c12e5a
zahrablend <zahra@LAPTOP-010BK67M>
2023-09-14 12:24:07 +0300

    Corrected naming according to conventions

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,16 +1,18 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+import jdk.jshell.spi.ExecutionControl;
 
-    private int AGE;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
